64ed479e1c7f47c8dd084359fd781b78994c6c1c
renananiceto <renan.a.ferreira94@gmail.com>
2021-06-20 14:18:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    valor: '',
+    valor_: '',
+    res:'',
+  }
+
+  handleChange = (eve,type) => {
+    this.setState({
+      [type]: Number(eve.target.value)
+    })
+  };
+
+  handleSub = () => {
+    this.setState({
+      res: this.state.valor - this.state.valor_
+    })
+  };
+
+  handleAdd = () => {
+    this.setState({
+      res: this.state.valor + this.state.valor_
+    })
+  };
+  handleMult = () => {
+    this.setState({
+      res: this.state.valor * this.state.valor_
+    })
+  };
+  handleDivi = () => {
+    this.setState({
+      res: this.state.valor / this.state.valor_
+    })
+  };
+
+
+
+  render(){
+      return(
+        <div>
+          <input placeholder='Digite um numero ' onChange={(eve) => this.handleChange (eve,'valor')} />
+          <input placeholder='Digite um numero ' onChange={(eve) => this.handleChange (eve,'valor_')} />
+          <button onClick={this.handleAdd}>+</button>
+          <button onClick={this.handleSub}>-</button>
+          <button onClick={this.handleDivi}>/</button>
+          <button onClick={this.handleMult}>*</button>
+          <h1>{this.state.res}</h1>
+        </div>
+      )
+    }
+
 }
 
 export default App;
